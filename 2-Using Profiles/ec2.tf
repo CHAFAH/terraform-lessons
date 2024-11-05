47234ba728e541cfb5520c79903ccd12bca01883
Chafah Sani <125821852+CHAFAH@users.noreply.github.com>
2024-11-05 20:05:30 +0100

    Update ec2.tf

--- 2-Using Profiles/ec2.tf
+++ 2-Using Profiles/ec2.tf
@@ -1,14 +1,13 @@
 # Below is the resource block which creates EC2 Instance
 resource "aws_instance" "prod-instance" {
-  ami           = ""
-  instance_type = ""
+  ami           = "ami-02db68a01488594c5"
+  instance_type = "t3.micro"
   tags = {
-    Name = ""
+    Name = "Terraform"
   }
 }
 
 # Below is the provider which helps in connecting with AWS Account
 provider "aws" {
-  region = ""
-  profile = ""
+  region = "eu-north-1"
 }
